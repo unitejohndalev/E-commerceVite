9cd594417dc69137807939e3e9050a38d40f2200
John Dale <unitejohndalev@gmail.com>
2023-07-05 18:11:54 +0800

    added SignIn PopUp, Added Feature Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,16 @@
 import React from "react";
 
 //import data
-import data from "../data/Acc.json";
 
-console.log(data);
+
+
 const Home = () => {
   //destructure data
-  const { bags } = data;
+
 
   return (
     <div className="h-[100vh] flex flex-col justify-center items-center">
-      {bags.map((bagproducts) => {
-        //destructure bagproducts
-        const { description, id } = bagproducts;
-     
-        return (
-          <div key={id} className=" mt-10">
-            <div>
-              {description.map((infobag) => {
-                const { info, img1 } = infobag;
-                return <div>{info}</div>;
-              })}
-            </div>
-          </div>
-        );
-      })}
+    <h1 className="text-[5rem]">HOME</h1>
     </div>
   );
 };
