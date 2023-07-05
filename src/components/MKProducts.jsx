4cc1bcfbc3bb554cd679b6e8e7771eb85f2e44aa
adeldiola <adelainediola@gmail.com>
2023-07-05 19:53:22 +0300

    added MKProducts fixed bugs

--- src/components/MKProducts.jsx
+++ src/components/MKProducts.jsx
@@ -1,32 +1,44 @@
 import React from "react";
 
-import MKProducts from "../data/MKProducts.json";
+import data from "../data/MKProducts.json";
 
 const MKProducts = () => {
-//destructure data
-    const {bags} = data;
-    console.log(data)
-    return (
-    <div className="h-[100vh] flex flex-col justify-center items-center">
-        {bags.map((MKbags) => {
-            //destructure data
-            const {name, id, color, desctription} =MKbags;
+  //destructure data
+  const { bags } = data;
 
-            return (
-                <div key={id} className="nt-10">
-                    <div>
-                        {desctription.map((infobags)  => {
-                            //destructure data
-                            const {info, img1} = infobags;
-                            return <div> {info} </div>;
+  return (
+    <div>
+      <div className=" ">
+        <div className="relative">
+          <div className="h-[100vh] overflow-auto ">
+            <div className=" space-x-4 mb-2 grid md:grid-cols-3 l:grid-cols-12 me-5 m-2 mt-[150px] transition-all">
+              {bags.map((mkbags) => {
+                const { id, img, name, price } = mkbags;
 
-                        })}
-                    </div>
-                    </div>
-            );
-        })}
+                return (
+                  <div
+                    key={id}
+                    className="relative flex flex-col items-center text-center"
+                  >
+                    <p className="text-base "></p>
+                    <img className="rounded-3xl w-48 h-45 " src={img}></img>
+                    <p className=" text-black text-center w-60 font-medium text-sm m-5">
+                      <p className="font-bold text-xs mb-2">Michael Kors</p>
+                      {name}
+                      <p className="font-light text-gray-800 mt-4">
+                        {" "}
+                        $ {price}
+                      </p>
+                    </p>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-);
+  );
 };
-export  default MKProducts
-       
+
+export default MKProducts;
