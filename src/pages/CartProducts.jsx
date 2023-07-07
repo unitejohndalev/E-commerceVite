a887448db03c0711d9f6718111a0a817d3ce90e7
John Dale <unitejohndalev@gmail.com>
2023-07-07 17:14:06 +0800

    remove unwanted function

--- src/pages/CartProducts.jsx
+++ src/pages/CartProducts.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 import Hat from '../components/Hat'
 
 const CartProducts = () => {
-    const addToCart = () => {
-        console.log('added to cart')
-    }
+ 
   return (
     <div>
         <Hat />
