8ad7a9b6e36e56bf7471b043d094300a4d9315c2
John Dale <unitejohndalev@gmail.com>
2023-07-13 14:52:56 +0800

    added productdescprodiver

--- src/main.jsx
+++ src/main.jsx
@@ -9,17 +9,22 @@ import { BrowserRouter as Router } from "react-router-dom";
 //import product provider and wrap app in order for children to access data within it
 import ProductProvider from "./contexts/ProductContext.jsx";
 
+//import product description provider
+
 //import cart provider and wrap product provider in order fot the cart to access all products
 import CartProvider from "./contexts/CartContext.jsx";
+import ProductDescriptionProvider from "./contexts/ProductDescriptionContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <CartProvider>
     <ProductProvider>
-      <React.StrictMode>
-        <Router>
-          <App />
-        </Router>
-      </React.StrictMode>
+      <ProductDescriptionProvider>
+        <React.StrictMode>
+          <Router>
+            <App />
+          </Router>
+        </React.StrictMode>
+      </ProductDescriptionProvider>
     </ProductProvider>
   </CartProvider>
 );
