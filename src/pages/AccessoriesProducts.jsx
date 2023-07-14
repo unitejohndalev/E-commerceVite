ebd7669ae95da24b2ca7ba4534126b4c7367ae75
John Dale <unitejohndalev@gmail.com>
2023-07-14 17:29:56 +0800

    nothing change

--- src/pages/AccessoriesProducts.jsx
+++ src/pages/AccessoriesProducts.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+
+
 const AccessoriesProducts = () => {
-  return <div className=""></div>;
+  return <div className="h-[100vh] flex justify-center items-center">
+  <div className="h-[90vh] mt-[150px]">
+
+ACCESSORIES
+ 
+  </div>
+  </div>;
 };
 
 export default AccessoriesProducts;
