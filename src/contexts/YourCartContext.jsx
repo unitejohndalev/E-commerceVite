1d512871cf7c99e8b0a84e0766ce6ef2b00bab86
John Dale <unitejohndalev@gmail.com>
2023-07-15 20:21:24 +0800

    added style

--- src/contexts/YourCartContext.jsx
+++ src/contexts/YourCartContext.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 
 //import useNavigate from react-dom
 //by using this hook we can navigate back programmatically
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 //import cart context
 import { CartContext } from "./CartContext";
@@ -11,6 +11,8 @@ import { Button } from "@material-tailwind/react";
 //import react icons
 import { IoMdAdd, IoMdRemove, IoMdClose } from "react-icons/io";
 import { FiTrash2 } from "react-icons/fi";
+
+import Footer from "../components/Footer";
 
 const YourCartContext = () => {
   //get cart state in cart context
@@ -31,90 +33,115 @@ const YourCartContext = () => {
   };
 
   return (
-    <div className="h-[100vh] overflow-hidden flex flex-col justify-center items-center">
-      {/* navigate back button */}
-      <button onClick={goBack} className="absolute left-2 lg:left-5 top-[110px] bg-red-600 text-white
-       p-2 rounded-xl z-10">
-        BACK
-      </button>
-      <div className="relative w-[500px]">
-        <div className="h-[70vh] relative overflow-auto mt-10 no-scrollbar">
-          {cart.map((hats, idx) => {
-            //destructure hats data
-            const { id, name, price, img, amount } = hats;
-            return (
-              <div key={idx} className="">
-                <div
-                  className="relative flex w-[500px] 
-            mt-5 shadow-lg rounded-xl"
-                >
-                  <div className="relative">
-                    <img
-                      src={img}
-                      alt="imgProduct"
-                      className="w-[100px] p-1 rounded-xl"
-                    />
-                  </div>
-                  <div className=" w-[100%] p-2 relative">
-                    <div className="flex absolute top-0 ">
-                      <h1 className="">{name}</h1>
-                    </div>
-                    <div
-                      className="absolute right-1 top-0"
-                      onClick={() => removeFromCart(id)}
-                    >
-                      <IoMdClose />
-                    </div>
-                    <div className="flex flex-row ">
+    <div className="h-[100%] ">
+      <div className="flex mt-[100px] justify-center items-center">
+        <div className="w-[100%] md:max-w-[768px] lg:max-w-[1040px] flex items-center justify-center">
+          {/* navigate back button */}
+          <button
+            onClick={goBack}
+            className="absolute left-2 lg:left-5 top-[110px] text-white
+       p-2 rounded-sm btn-bg z-10"
+          >
+            BACK
+          </button>
+          <div className="relative w-[90%] mt-[60px] h-[100%] ">
+            {cart.length === 0 ? (
+              <div className=" flex justify-center items-center h-[50vh]">
+                <p className="text-[2rem] font-medium">Your Cart is empty!</p>
+              </div>
+            ) : (
+              <div className="h-[80%] relative overflow-auto no-scrollbar mt-10 flex flex-col shadow-xl ">
+                {cart.map((products, idx) => {
+                  //destructure hats data
+                  const { id, name, price, img, amount } = products;
+                  return (
+                    <div key={idx} className="">
                       <div
-                        className="flex justify-center items-center gap-x-5 absolute
-                  bottom-1"
+                        className="relative flex w-[100%] 
+            mt-5 shadow-lg rounded-sm"
                       >
-                        <Button
-                          className="bg-white text-black"
-                          onClick={() => decreaseAmount(id)}
-                        >
-                          <IoMdRemove />
-                        </Button>
-                        <p>{amount}</p>
-                        <Button
-                          className="bg-white text-black"
-                          onClick={() => increaseAmount(id)}
-                        >
-                          <IoMdAdd />
-                        </Button>
+                        <div className="relative">
+                          <Link to={`/allproductsearch/${id}`}>
+                            <img
+                              src={img}
+                              alt="imgProduct"
+                              className="w-[100px] p-1 rounded-sm"
+                            />
+                          </Link>
+                        </div>
+                        <div className=" w-[100%] p-2 relative">
+                          <div className="flex absolute top-0 ">
+                            <h1 className="">{name}</h1>
+                          </div>
+                          <div
+                            className="absolute right-1 top-0"
+                            onClick={() => removeFromCart(id)}
+                          >
+                            <IoMdClose />
+                          </div>
+                          <div className="flex flex-row ">
+                            <div
+                              className="flex justify-center items-center gap-x-2 absolute
+                  bottom-1"
+                            >
+                              <Button
+                                className="bg-white text-black !rounded-sm"
+                                onClick={() => decreaseAmount(id)}
+                              >
+                                <IoMdRemove />
+                              </Button>
+                              <div className="w-[20px] text-center">
+                                <p className="">{amount}</p>
+                              </div>
+                              <Button
+                                className="bg-white text-black !rounded-sm"
+                                onClick={() => increaseAmount(id)}
+                              >
+                                <IoMdAdd />
+                              </Button>
+                            </div>
+                          </div>
+                          <div
+                            className=" w-[55%] absolute right-0
+                  flex justify-between bottom-1"
+                          >
+                            <p className="ml-2">$ {price}</p>
+
+                            <p className=" absolute right-1">{`$ ${parseFloat(
+                              price * amount
+                            ).toFixed(2)}`}</p>
+                          </div>
+                        </div>
                       </div>
                     </div>
-                    <div
-                      className=" w-[55%] absolute right-0
-                  flex justify-between bottom-1"
-                    >
-                      <p className="">$ {price}</p>
-
-                      <p className=" absolute right-1">{`$ ${parseFloat(
-                        price * amount
-                      ).toFixed(2)}`}</p>
-                    </div>
-                  </div>
+                  );
+                })}
+              </div>
+            )}
+            {/* clear all products in cart */}
+            {cart.length === 0 ? (
+              <></>
+            ) : (
+              <div className="mt-[100px]">
+                <div
+                  className="cursor-pointer py-4 bg-red-500
+           text-white w-12 h-12 flex justify-center text-sm rounded-sm mt-5 
+          absolute bottom-0  right-0"
+                  onClick={clearCart}
+                >
+                  <FiTrash2 />
+                </div>
+                {/* products cart total */}
+                <div className="uppercase font-semibold absolute bottom-0 left-0">
+                  <span className="mr-2">Total:</span>${" "}
+                  {parseFloat(total).toFixed(2)}
                 </div>
               </div>
-            );
-          })}
-        </div>
-        {/* clear all products in cart */}
-        <div
-          className="cursor-pointer py-4 bg-red-500
-           text-white w-12 h-12 flex justify-center text-xl rounded-xl mt-5 
-          absolute -bottom-[80px] right-0"
-          onClick={clearCart}
-        >
-          <FiTrash2 />
-        </div>
-        {/* products cart total */}
-        <div className="uppercase font-semibold absolute -bottom-[80px] left-0">
-          <span className="mr-2">Total:</span>$ {parseFloat(total).toFixed(2)}
+            )}
+          </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
