9515476e49ef2647cd32e2f4cacfd28c57bafee0
John Dale <unitejohndalev@gmail.com>
2023-07-16 19:47:14 +0800

    fix UI

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,13 +15,28 @@ import Logo from "./img/logo.png";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
-  const [showShop, setShowShop] = useState(false);
+  const [showShop, setShowShop] = useState();
   const showToggle = () => {
-    setShowShop((prev) => !prev);
+    setShowShop(prev => ! prev);
+    setShowHelp(false)
+    setAboutUs(false)
   };
+  const [aboutUs, setAboutUs] = useState()
+  const aboutToggle = () => {
+    setAboutUs(prev => !prev)
+    setShowShop(false)
+    setShowHelp(false)
+  }
+
+  const [showHelp, setShowHelp] = useState()
+  const helpToggle = () => {
+    setShowHelp(prev => !prev)
+    setShowShop(false)
+    setAboutUs(false)
+  }
   return (
     // change footer background color
-    <footer className="bg-primary relative p-10 h-[60vh] mt-10">
+    <footer className="bg-primary relative p-10 h-[60vh] lg:h-[50vh] mt-10">
       <div className="lg:w-[1240px] h-[25vh] m-auto flex justify-center -mt-8 ">
         <div
           className="w-[100%] flex flex-col justify-center lg:w-[1240px] lg:flex-row relative lg:justify-between
@@ -44,70 +59,110 @@ const Footer = () => {
                 </div>
               )}
             </div>
-            <div className="hidden lg:flex  ">
-              <div className=" text-white w-[1240px] flex justify-between absolute top-0">
-                <p className="text-[1.3rem] font-light">Shop</p>
+
+            <div className="hidden lg:flex bg-yellow-800 ">
+              <div className=" text-white w-[1240px] flex justify-between absolute top-0 ">
+                <p className="text-[1.3rem] font-medium">Shop</p>
                 <div className="flex flex-col gap-y-1 mt-2 absolute left-0 top-[30px]">
                   <div className="w-[50px]">
-                    <p className="font-thin">Hats</p>
+                    <p className="font-medium text-[.8rem] text-white/60 hover:text-white">
+                      Hats
+                    </p>
                   </div>
                   <div className="w-[50px]  ">
-                    <p className="font-thin ">Cloths</p>
+                    <p className="font-medium text-[.8rem] text-white/60 hover:text-white">
+                      Cloths
+                    </p>
                   </div>
                   <div className="w-[50px] ">
-                    <p className="font-thin ">Shoes</p>
+                    <p className="font-medium text-[.8rem] text-white/60 hover:text-white">
+                      Shoes
+                    </p>
                   </div>
                   <div className="w-[50px]  ">
-                    <p className="font-thin ">Bags</p>
+                    <p className="font-medium text-[.8rem] text-white/60 hover:text-white">
+                      Bags
+                    </p>
                   </div>
                   <div className="w-[50px]">
-                    <p className="font-thin ">Accessories</p>
+                    <p className="font-medium text-[.8rem] text-white/60 hover:text-white">
+                      Accessories
+                    </p>
                   </div>
                 </div>
-                <div className="hidden lg:flex w-[300px]">
-                  <p className="text-[1.3rem] font-light">About Us</p>
+                <div className="hidden lg:flex absolute left-[270px] w-[300px]">
+                  <p className="text-[1.3rem] font-medium">About Us</p>
                   <div className="flex flex-col gap-y-1 mt-2 absolute top-[30px]">
                     <div className="w-[50px]">
-                      <p className="font-thin">text</p>
+                      <p className="font-medium text-[.8rem] text-white/60 hover:text-white">
+                        News
+                      </p>
                     </div>
                     <div className="w-[50px]  ">
-                      <p className="font-thin ">text</p>
+                      <p className="font-medium text-[.8rem] text-white/60 hover:text-white">
+                        Careers
+                      </p>
                     </div>
                     <div className="w-[50px] ">
-                      <p className="font-thin ">text</p>
+                      <p className="font-medium text-[.8rem] text-white/60 hover:text-white">
+                        Investors
+                      </p>
                     </div>
                     <div className="w-[50px]  ">
-                      <p className="font-thin ">text</p>
+                      <p className="font-medium text-[.8rem] text-white/60 hover:text-white">
+                        Purpose
+                      </p>
                     </div>
                     <div className="w-[50px]">
-                      <p className="font-thin ">text</p>
+                      <p className="font-medium text-[.8rem] text-white/60 hover:text-white">
+                        Sustainability
+                      </p>
                     </div>
                   </div>
                 </div>
 
-                <div className="hidden lg:flex w-[400px]">
-                  <p>Contact Us</p>
+                <div className="hidden lg:flex absolute left-[600px] w-[200px]">
+                  <p className="text-[1.3rem] font-medium">Get Help</p>
                   <div className="flex flex-col gap-y-1 mt-2 absolute top-[30px]">
-                    <div className="w-[50px]">
-                      <input type="text" />
+                    <div className="w-[150px]">
+                      <p className="font-medium text-[.8rem] text-white/60 hover:text-white">
+                        Order Status
+                      </p>
                     </div>
-                    <div className="w-[50px]  ">
-                    <input type="text" />
+                    <div className="w-[150px]  ">
+                      <p className="font-medium text-[.8rem] text-white/60 hover:text-white">
+                        Shipping and Delivery
+                      </p>
                     </div>
                     <div className="w-[50px] ">
-                  <textarea name="" id="" ></textarea>
+                      <p className="font-medium text-[.8rem] text-white/60 hover:text-white">
+                        Returns
+                      </p>
                     </div>
-                    <div className="w-[50px]  ">
-                    <button className="btn-bg px-5">Send</button>
+                    <div className="w-[150px]  ">
+                      <p className="font-medium text-[.8rem] text-white/60 hover:text-white">
+                        Order Cancellation
+                      </p>
                     </div>
-                  
+                    <div className="w-[150px]">
+                      <p className="font-medium text-[.8rem] text-white/60 hover:text-white">
+                        Contact Us
+                      </p>
+                    </div>
                   </div>
+                </div>
+                <div className="flex justify-evenly text-[1.5rem] w-[320px] cursor-pointer">
+                  <FaFacebookF className="hover:text-white/50 " />
+                  <FaTwitter className="hover:text-white/50 " />
+                  <FaInstagram className="hover:text-white/50 " />
+                  <FaYoutube className="hover:text-white/50 " />
+                  <FaPinterest className="hover:text-white/50 " />
                 </div>
               </div>
             </div>
 
             {showShop && (
-              <div className="w-[100%] absolute top-[55px] flex flex-col gap-y-2 z-20 bg-primary mt-2 lg:hidden">
+              <div className="w-[100%] absolute top-[55px] flex flex-col gap-y-2 z-20 bg-primary mt-5 lg:hidden">
                 <div className=" p-2 bg-white rounded-sm hover:ml-2 hover:bg-white/80 active:bg-white">
                   Hats
                 </div>
@@ -126,16 +181,82 @@ const Footer = () => {
               </div>
             )}
           </div>
-          <div className="h-[100%] flex items-center cursor-pointer relative w-[100%] lg:hidden">
-            <div className="w-[100%] px-2 py-2 rounded-sm bg-white hover:bg-white/80 active:bg-white lg:w-[300px] lg:absolute lg:top-0 lg:-right-[150px]">
+          {showShop === true ? (
+            <div className="h-[13vh]"></div>
+          ) : (
+            <div
+              className="w-[100%] cursor-pointer flex justify-between items-center px-2 py-1 rounded-sm bg-white
+              lg:hidden"
+              onClick={aboutToggle}
+            >
               <p className="">About Us</p>
+              {aboutUs === false ? (
+                <div className=" text-[2rem] ">
+                  <VscChevronDown />
+                </div>
+              ) : (
+                <div className=" text-[2rem] ">
+                  <VscChevronUp />
+                </div>
+              )}
             </div>
-          </div>
-          <div className="h-[100%] flex items-center cursor-pointer relative w-[100%] lg:hidden">
-            <div className="w-[100%] px-2 py-2 rounded-sm bg-white hover:bg-white/80 active:bg-white lg:w-[300px] lg:absolute lg:top-0 lg:right-0">
-              <p className="">Contact Us</p>
+          )}
+          {aboutUs && (
+            <div className="w-[100%] absolute top-[125px] flex flex-col gap-y-2 z-20 bg-primary mt-2 cursor-pointer lg:hidden">
+              <div className=" p-2 bg-white rounded-sm hover:ml-2 hover:bg-white/80 active:bg-white">
+                News
+              </div>
+              <div className=" p-2 bg-white rounded-sm hover:ml-2 hover:bg-white/80 active:bg-white">
+                Careers
+              </div>
+              <div className=" p-2 bg-white rounded-sm hover:ml-2 hover:bg-white/80 active:bg-white">
+                Investors
+              </div>
+              <div className=" p-2 bg-white rounded-sm hover:ml-2 hover:bg-white/80 active:bg-white">
+                Purpose
+              </div>
+              <div className=" p-2 bg-white rounded-sm hover:ml-2 hover:bg-white/80 active:bg-white">
+                Sustainability
+              </div>
             </div>
-          </div>
+          )}
+        {aboutUs === true ? (<div className="h-[25vh]"></div>) : (<div className="h-[100%] flex items-center cursor-pointer relative w-[100%] lg:hidden">
+            <div
+              className="w-[100%] flex justify-between items-center px-2 py-1 rounded-sm bg-white
+              lg:hidden"
+              onClick={helpToggle}
+            >
+              <p className="">Get Help</p>
+              {showHelp === false ? (
+                <div className=" text-[2rem] ">
+                  <VscChevronDown />
+                </div>
+              ) : (
+                <div className=" text-[2rem] ">
+                  <VscChevronUp />
+                </div>
+              )}
+            </div>
+          </div>)  }
+          {showHelp && (
+            <div className="w-[100%] absolute top-[185px] flex flex-col gap-y-2 z-20 bg-primary mt-2 cursor-pointer lg:hidden">
+              <div className=" p-2 bg-white rounded-sm hover:ml-2 hover:bg-white/80 active:bg-white">
+                Order Status
+              </div>
+              <div className=" p-2 bg-white rounded-sm hover:ml-2 hover:bg-white/80 active:bg-white">
+                Shipping and Delivery
+              </div>
+              <div className=" p-2 bg-white rounded-sm hover:ml-2 hover:bg-white/80 active:bg-white">
+                Returns
+              </div>
+              <div className=" p-2 bg-white rounded-sm hover:ml-2 hover:bg-white/80 active:bg-white">
+                Order Cancellation
+              </div>
+              <div className=" p-2 bg-white rounded-sm hover:ml-2 hover:bg-white/80 active:bg-white">
+                Contact Us
+              </div>
+            </div>
+          )}
         </div>
         <div
           className="text-white flex flex-col justify-center items-center absolute bottom-2 w-[90%] m-auto
@@ -143,14 +264,14 @@ const Footer = () => {
         >
           <div className="h-[100%] flex flex-col items-center justify-between">
             {/* change icon color */}
-            <div className="flex justify-evenly text-[1.2rem] md:text-[1rem] w-[420px] cursor-pointer ">
+            <div className="flex justify-evenly text-[1.2rem] md:text-[1rem] w-[420px] cursor-pointer lg:hidden">
               <FaFacebookF className="hover:text-black " />
               <FaTwitter className="hover:text-black " />
               <FaInstagram className="hover:text-black " />
               <FaYoutube className="hover:text-black " />
               <FaPinterest className="hover:text-black " />
             </div>
-            <div className="w-[90%] lg:w-[500px]">
+            <div className="w-[90%] lg:w-[500px] lg:mt-10">
               <p className="text-[.7rem] text-center font-light">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                 Necessitatibus error optio nihil et doloribus eveniet rerum sed
