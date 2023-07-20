ddf4a9f861ab0ba32892e473db93ada7f57dcf55
adeldiola <adelainediola@gmail.com>
2023-07-21 00:41:33 +0300

    Register styled modified

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "../styles/Register.css";
 import { TextField, Stack } from "@mui/material";
 import { Button } from "@material-tailwind/react";
 import { Link } from "react-router-dom";
@@ -17,73 +18,103 @@ const Register = () => {
 
   return (
     <React.Fragment>
-      <h2>Register Form</h2>
-      <form onSubmit={handleSubmit} action={<Link to="/login" />}>
-        <Stack spacing={2} direction="row" sx={{ marginBottom: 4 }}>
-          <TextField
-            type="text"
-            variant="outlined"
-            color="secondary"
-            label="first name"
-            onChange={(e) => setFirstName(e.target.value)}
-            value={firstName}
-            fullWidth
-            required
-          />
+      <div className="container mt-15 lg:mt-10 mx-auto py-40">
+        <div className="flex flex-col lg:flex-row bg-deep-orange-50 rounded-lg mx-auto shadow-xl overflow-hidden   lg:h-[550px] ">
+          <div className="w-[350px] lg:w-1/2 lg:p-2 h-[400px] ml-[-1rem] ">
+            <h2 className="font-semibold text-center text-black text-md lg:text-2xl mt-7 mb-5 lg:mt-6 lg:mb-5 lg:p-5 ">
+              Register Form
+            </h2>
+            <form onSubmit={handleSubmit} action={<Link to="/login" />}>
+              <Stack
+                spacing={1}
+                direction="column"
+                sx={{ marginBottom: 4, ml: 8 }}
+              >
+                <TextField
+                  type="text"
+                  variant="outlined"
+                  color="secondary"
+                  label="First name"
+                  size="small"
+                  onChange={(e) => setFirstName(e.target.value)}
+                  value={firstName}
+                  required
+                  sx={{ minWidth: "20%", maxWidth: "80%" }}
+                />
 
-          <TextField
-            type="text"
-            variant="outlined"
-            color="secondary"
-            label="lastName"
-            onChange={(e) => setLastName(e.target.value)}
-            required
-          />
-        </Stack>
+                <TextField
+                  type="text"
+                  variant="outlined"
+                  color="secondary"
+                  label="LastName"
+                  size="small"
+                  onChange={(e) => setLastName(e.target.value)}
+                  required
+                  sx={{ minWidth: "20%", maxWidth: "80%" }}
+                />
 
-        <TextField
-          type="email"
-          variant="outlined"
-          color="secondary"
-          label="Email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-          fullWidth
-          required
-          sx={{ mb: 4 }}
-        />
+                <TextField
+                  type="email"
+                  variant="outlined"
+                  color="secondary"
+                  label="Email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                  size="small"
+                  required
+                  sx={{ minWidth: "20%", maxWidth: "80%" }}
+                />
 
-        <TextField
-          type="password"
-          variant="outlined"
-          color="secondary"
-          label="Password"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-          required
-          fullWidth
-          sx={{ mb: 4 }}
-        />
+                <TextField
+                  type="password"
+                  variant="outlined"
+                  color="secondary"
+                  label="Password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                  required
+                  size="small"
+                  sx={{ minWidth: "20%", maxWidth: "80%" }}
+                />
 
-        <TextField
-          type="date"
-          variant="outlined"
-          color="secondary"
-          label="Date of Birth"
-          onChange={(e) => setDateBirth(e.target.value)}
-          value={dateBirth}
-          fullWidth
-          required
-          sx={{ mb: 4 }}
-        />
+                <TextField
+                  type="date"
+                  variant="outlined"
+                  color="secondary"
+                  label="Date of Birth"
+                  onChange={(e) => setDateBirth(e.target.value)}
+                  value={dateBirth}
+                  size="small"
+                  required
+                  sx={{ minWidth: "20%", maxWidth: "80%" }}
+                />
+              </Stack>
 
-        <Button variant="outline" color="secondary" type="submit">
-          Register
-        </Button>
-      </form>
-      <small>
-        Already have an account? <Link to="/login"> Login here.</Link>
-      </small>
+              <div className="flex-col flex items-center justify-center  lg:gap-2 py-4">
+                <Button variant="outline" color="secondary" type="submit">
+                  Register
+                </Button>
+              </div>
+            </form>
+            <p className=" text-black text-center text-xs lg:text-sm gap-3 lg:mt-6 mt-1">
+              {" "}
+              Already have an account?
+              <span className="underline">
+                <Link to="/login"> Login here.</Link>
+              </span>
+            </p>
+          </div>
+
+          <div className="w-full lg:w-[1/2] lg:mb-12 md:mb-0 max-w-3xl flex flex-col justify-center bg-imageReg bg-no-repeat bg-cover bg-center h-[250px] lg:h-[550px] ">
+            <h1 className=" text-black text-center  text-2xl lg:text-4xl font-medium">
+              Welcome to Shoppy!
+            </h1>
+            <p className="text-black text-center">
+              Be a member and receive rewards!
+            </p>
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
